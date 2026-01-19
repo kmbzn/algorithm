bffb12dad21ca0a66c4448d6db152c102a6a5dca
KeyBaseZone <kmbzn24@gmail.com>
2026-01-19 22:09:15 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2412 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int N, M;
+    cin >> N >> M;
+    vector<int> P(N);
+    int p;
+    for (int i = 1; i <= N; ++i) {
+        cin >> p;
+        P[i] = P[i - 1] + p;
+    }
+
+    for (int i = 0; i < M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        cout << P[b] - P[a - 1] << "\n";
+    }
+    return 0;
+}
