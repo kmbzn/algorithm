5109c4f25f9f40564c63edadfdafe268b8eaf551
KeyBaseZone <kmbzn24@gmail.com>
2026-02-20 22:22:53 +0900

    [Silver IV] Title: 수 찾기, Time: 404 ms, Memory: 52432 KB -BaekjoonHub

--- Python/백준/Silver/1920. 수 찾기/수 찾기.py
+++ Python/백준/Silver/1920. 수 찾기/수 찾기.py
@@ -2,9 +2,8 @@ import sys
 
 N = int(sys.stdin.readline())
 
-A = list(map(int, sys.stdin.readline().split()))
+A = set(map(int, sys.stdin.readline().split()))
 
-A.sort()
 
 M = int(sys.stdin.readline())
 
@@ -12,18 +11,7 @@ B = list(map(int, sys.stdin.readline().split()))
 
 res = ""
 
-def find(start, end, item):
-    if end - start == 1:
-        if A[start] == item: return 1
-        else: return 0;
-
-    mid = (start + end + 1) // 2
-    if item < A[mid]:
-        return find(start, mid, item)
-    else:
-        return find(mid, end, item)
-
 for i in B:
-    res += str(find(0, N, i)) + "\n"
+    res += str(int(i in A)) + "\n"
 
 print(res)
