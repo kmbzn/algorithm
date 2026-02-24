df1f11e00b6d989c145fc322e033762f527ff413
KeyBaseZone <kmbzn24@gmail.com>
2026-02-24 15:57:28 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -0,0 +1,17 @@
+zero = [0] * 41
+one = [0] * 41
+
+zero[0] = 1
+zero[1] = 0
+
+one[0] = 0
+one[1] = 1
+for i in range(2, 41):
+    zero[i] = zero[i - 1] + zero[i - 2]
+    one[i] = one[i - 1] + one[i - 2]
+
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    print(str(zero[n]) + " " + str(one[n]))
