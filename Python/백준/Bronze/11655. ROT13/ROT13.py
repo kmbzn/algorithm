5a0107d3e1f027f49a5c8ee27cb7c28f918e3758
KeyBaseZone <kmbzn24@gmail.com>
2026-02-26 13:27:51 +0900

    [Bronze I] Title: ROT13, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/11655. ROT13/ROT13.py
@@ -0,0 +1,9 @@
+S = input()
+
+for i in S:
+    if 'A' <= i <= 'Z':
+        print(chr((ord(i) - ord('A') + 13) % 26 + ord('A')), end = '')
+    elif 'a' <= i <= 'z':
+        print(chr((ord(i) - ord('a') + 13) % 26 + ord('a')), end = '')
+    elif '0' <= i <= '9' or i == ' ':
+        print(i, end = '')
