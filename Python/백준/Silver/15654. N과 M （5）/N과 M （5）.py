6594e71d1776081be009a7b0bd7d4c5d5abc296c
KeyBaseZone <kmbzn24@gmail.com>
2026-03-03 17:25:30 +0900

    [Silver III] Title: N과 M (5), Time: 196 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+
+N, M = map(int, input().split())
+
+L = list(map(int, input().split()))
+
+L.sort()
+
+for i in permutations(L, M):
+    for j in i:
+        print(j, end = ' ')
+    print()
